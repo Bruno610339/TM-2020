b543c6832ce9fa51f5e00dc44a64403823e2e9ba
Bruno610339 <bmiguelgomes@ipvc.pt>
2020-04-26 22:08:21 +0100

    TP1-22253-21586

---
+++ TP1 Unity/TP1-22253-21586/Sci-Fi Shooter/Assets/Shield.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+
+    public int hitsNeeded = 10;
+    public int hitsTaken;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        hitsTaken += 1;
+
+        if (hitsTaken >= hitsNeeded)
+            Destroy(gameObject);
+    }
+}
